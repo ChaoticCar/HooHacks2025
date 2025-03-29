8d4efa746247c7dc74d0ffc00c04a5aa510aa918
ChaoticCar14 <ChaoticCar14@gmail.com>
2025-03-29 13:22:05 -0400

    Extra GUI Things

--- Application.java
+++ Application.java
@@ -16,7 +16,7 @@ class Application {
     
     private static void initWindow() {
         // create a window frame and set the title in the toolbar
-        JFrame window = new JFrame("Can't Stop, Won't Stop, GameStop");
+        JFrame window = new JFrame("GUI Window");
         // when we close the window, stop the app
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
--- GameCanvas.java
+++ GameCanvas.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,9 +9,40 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
 
 public class GameCanvas extends JPanel implements ActionListener, KeyListener, MouseListener {
 
+    // controls the delay between each tick in ms
+    private final int DELAY = 25;
+    // controls the size of the board
+    public static final int TILE_SIZE = 50;
+    public static final int ROWS = 12;
+    public static final int COLUMNS = 18;
+
+    // suppress serialization warning
+    private static final long serialVersionUID = 490905409104883233L;
+    
+    // keep a reference to the timer object that triggers actionPerformed() in
+    // case we need access to it in another method
+    private Timer timer;
+ 
+
+    public GameCanvas(){
+        // set the game board size
+        setPreferredSize(new Dimension(TILE_SIZE * COLUMNS, TILE_SIZE * ROWS));
+        // set the game board background color
+        setBackground(new Color(232, 232, 232));
+
+        // initialize the game state
+      
+
+        // this timer will call the actionPerformed() method every DELAY ms
+        timer = new Timer(DELAY, this);
+        timer.start();
+        
+    }
     @Override
     public void paint(Graphics g) {
         super.paint(g);
